0dd8c369e582e3e3abeaff38c8da1ede84b77ca3
homma509 <kouki.honma@gmail.com>
2021-01-16 17:05:12 +0900

    style: main.goを移動したため、build scriptで指定しているmain.goのパスを修正

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "-s -w" -o ./build/9rece ./api/main.go
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "-s -w" -o ./build/9rece ./main.go
